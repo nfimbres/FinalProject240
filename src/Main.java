aee92739587d5e58f3c592eb88cdee36dfad9877
willtoledosimp <nfimbres@mail.umw.edu>
2023-04-11 19:30:13 -0400

    methods created

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,21 @@ import java.util.ArrayList;
 
 public class Main {
     public static void Main(String args[]) {
+        JFrame frame = new JFrame("Playlister");
     }
     public static void addLabel(String text, JFrame j) {
+        JLabel label = new JLabel(text);
+        j.getContentPane().add(label);
     }
     public static void addButton(String text, JFrame j) {
+        JButton button = new JButton(text);
+        button.addActionListener(new ButtonListener());
+        j.getContentPane().add(button);
     }
     public static void addButton(String text, JFrame j, JTextField t) {
+        JButton button = new JButton(text);
+        button.addActionListener(new TextFieldListener(t));
+        j.getContentPane().add(button);
     }
     public static void newPlaylist(){
     }
