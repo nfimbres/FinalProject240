074ea11c72643de8036e02e84adb34a57e1e6420
willtoledosimp <nfimbres@mail.umw.edu>
2023-04-11 14:39:23 -0400

    methods created

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void Main(String args[]) {
+    }
+    public static void addLabel(String text, JFrame j) {
+    }
+    public static void addButton(String text, JFrame j) {
+    }
+    public static void addButton(String text, JFrame j, JTextField t) {
+    }
+    public static void newPlaylist(){
+    }
+    public static void editPlaylist(){
+    }
+    public static void shufflePlaylist(){
+    }
+    public static void reorderPlaylist(){
+    }
+    public static ArrayList<String> getPlaylists(){
+        return new ArrayList<>();
+    }
+    public static Playlist choosePlaylist(){
+        return new Playlist();
+    }
+    public static void ask(){
+    }
+    public static void exit(){
+    }
+}
