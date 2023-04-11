a579a0f96f255a50d42fbae502cbde6dbda0e0f1
willtoledosimp <nfimbres@mail.umw.edu>
2023-04-11 14:53:51 -0400

    methods created

--- src/TextFieldListener.java
+++ src/TextFieldListener.java
@@ -2,7 +2,11 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ButtonListener implements ActionListener {
+public class TextFieldListener implements ActionListener {
+    private JTextField textField; // variable for text field
+    public TextFieldListener(JTextField t) { //constructor
+        textField = t;
+    }
     @Override
     public void actionPerformed(ActionEvent e) { // method for when the button is clicked
 
